27235c41985400295ddb0679537b756b781c1e41
Amita <amita.parikh@gmail.com>
2019-03-12 22:06:25 +0000

    Add a generate random string function

--- express_server.js
+++ express_server.js
@@ -4,6 +4,10 @@ var PORT = 8080; //default port is 8080
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+
+}
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
