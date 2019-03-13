8403e38c70da34804f08abd9c269898eeec3e175
Amita <amita.parikh@gmail.com>
2019-03-13 22:17:15 +0000

    Display passed-in username next to form

--- express_server.js
+++ express_server.js
@@ -43,7 +43,7 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
