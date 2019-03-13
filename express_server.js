4940bf17fe524ef4f80466146e924b52236cb322
Amita <amita.parikh@gmail.com>
2019-03-13 16:09:21 +0000

    Added POST route for delete

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var PORT = 8000; //default port is 8080
+var PORT = 8080; //default port is 8080
 
 app.set("view engine", "ejs");
 
@@ -53,6 +53,14 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  let deletedURL = [req.params.shortURL];
+  // console.log(deletedURL); logging each step so I can remember what I did
+  delete urlDatabase[deletedURL];
+  //The reason we use urlDatabase[deletedURL] and not delete urlDatabase.deletedURL is because we can't pass a variable in a previously defined object
+    res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
